3a84f9e2f671dfd6bac2e65216fa49f3c65459cc
Michal Stabrawa <mistaaqua@gmail.com>
2018-04-22 16:54:16 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(window).load(function(){
+           $('#p2').animate({width: '150px'},3000);
+    })
+    
+   var a=$(window).offsetTop();
+    $(a).scrollTop(function(){
+        alert(a);
+    })
+})
+  
+   
+    
+  
+   
+    
+    	
+
+
