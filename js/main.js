52dcf057eaf6dc2cc99f5d93b5c5cf34a7687336
Michal Stabrawa <mistaaqua@gmail.com>
2018-04-26 08:26:45 +0200

    new section

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
-$(document).ready(function(){
-    $(window).load(function(){
-           $('#p2').animate({width: '150px'},3000);
-    })
-    
-   
-    
-    $('.btn-load').click(function(){
-        $('#triangle').css("display","none");
-    })
-})
   
-   
-    
-  
-   
-    
-    	
+
+    $(document).ready(function () {
+        $(window).load(function () {
+            $('#p2').animate({
+                width: '150px'
+            }, 3000);
+        })
 
 
+
+        $('.btn-load').click(function () {
+            $('#triangle').css("display", "none");
+        })
+
+
+    })
