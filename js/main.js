0274e9f452524e9ac31703586a7d396df100e4ba
Michal Stabrawa <mistaaqua@gmail.com>
2018-04-29 19:33:06 +0200

    rwd 1196

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.hamburger').click(function(){
         $('#menu').toggle();
     })
+    
+  $('#load-more-foto').click(function(){
+      $('.foto2').toggle("slow");
+  })
 })
